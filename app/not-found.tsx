6acbd51bf000095e006058abf5102d61c0d459ac
morit <moritz.brandner@student.htldornbirn.at>
2025-03-05 14:41:49 +0100

    first commit

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export default function NotFound({ }: Props) {
+    return (
+        <div className="flex justify-center align-middle text-red-600 text-9xl">
+            <h1>Problem with da code man</h1>
+        </div>
+    );
+}
